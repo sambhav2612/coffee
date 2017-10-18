6a19658ae029fed235fc3075d2843a71c7754008
sambhav.github.io <sambhavjain2612@gmail.com>
2017-10-18 09:48:50 +0530

    improved backend logic, added notifications support

---
+++ app/src/main/java/com/example/android/justjava/NotificationHelper.java
@@ -0,0 +1,37 @@
+package com.example.android.justjava;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.NotificationCompat;
+
+/**
+ * Created by Sambhav Jain on 10/18/2017.
+ * @author Sambhav Jain
+ */
+
+public class NotificationHelper extends AppCompatActivity {
+
+    int mNotificationId = 0;
+    android.support.v4.app.NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(NotificationHelper.this);
+
+    Intent intent = new Intent(NotificationHelper.this, MainActivity.class);
+    PendingIntent pIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+    public NotificationHelper(String subject, String message) {
+        mBuilder = new NotificationCompat.Builder(NotificationHelper.this)
+                        .setSmallIcon(R.drawable.logo)
+                        .setContentTitle(subject)
+                        .setContentText(message);
+
+        mBuilder.setContentIntent(pIntent);
+
+        // Sets an ID for the notification
+        mNotificationId++;
+        // Gets an instance of the NotificationManager service
+        NotificationManager mNotifyMgr = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        // Builds the notification and issues it.
+        mNotifyMgr.notify(mNotificationId, mBuilder.build());
+    }
+}
---
+++ app/src/main/java/com/example/android/justjava/credentials_main.java
@@ -0,0 +1,84 @@
+package com.example.android.justjava;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+/**
+ * Created by Sambhav Jain on 10/17/2017.
+ * @author Sambhav Jain
+ */
+
+public class credentials_main extends AppCompatActivity {
+
+    private EditText ed = null;
+    private EditText ed2 = null;
+
+    private String name;
+    private String email;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.credentials_main);
+
+        ed = (EditText) findViewById(R.id.name);
+        ed2 = (EditText)findViewById(R.id.email);
+
+        name = ed.getText().toString();
+        email = ed2.getText().toString();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("Application started", "");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("Application paused", "");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("Application resumed", "");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("Application stopped", "");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("Application destroyed", "");
+    }
+
+    public void gotomain(View view) {
+        /*if (name.equals("") & email.equals("")){
+            if (name.equals("")) {
+                ed.setError("name can not be left empty!");
+                ed.requestFocus();
+            }
+            if (email.equals("")) {
+                ed2.setError("email can not be left empty!");
+                ed2.requestFocus();
+            }
+        }
+        else {*/
+
+            Intent intent = new Intent(credentials_main.this, MainActivity.class);
+            intent.putExtra("name", name);
+            intent.putExtra("email", email);
+            startActivity(intent);
+        //}
+    }
+}
